681913a3683a57b625e25ea2cf440626500855f1
sselinatoledo <sselinatoledo@gmail.com>
2023-04-26 22:27:50 +0000

    fixing the npm issue

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
-let balance = 500.00;
+let balance = 500.00
 
 class Withdrawal {
 
   constructor(amount) {
-    this.amount = amount;
+    this.amount = amount
   }
 
   commit() {
-    balance -= this.amount;
+    balance -= this.amount
   }
 
 }
 
+// Driver Code
+t1 = new Withdrawal(50.25)
+t1.commit()
+console.log('Transaction 1:', t1)
 
+t2 = new Withdrawal(9.99)
+t2.commit()
+console.log('Transaction 2:', t2)
 
-
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
-
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
-
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
-
-console.log('Balance:', balance);
+console.log('Balance:', balance)
